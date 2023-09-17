b708eb184b4ee6f1c27b6a9953afb326e42aad6f
Jagad R R <43224845+zenthicmc@users.noreply.github.com>
2023-09-17 13:54:39 +0700

    Update overview.blade.php

--- resources/views/dashboard/overview.blade.php
+++ resources/views/dashboard/overview.blade.php
@@ -57,7 +57,7 @@
 							<div class="row">
 								<div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
 									<div class="stats-icon blue mb-2">
-										<i class="fa fa-bookmark" aria-hidden="true"></i>
+										<i class="fa fa-shopping-bag" aria-hidden="true"></i>
 									</div>
 								</div>
 								<div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
