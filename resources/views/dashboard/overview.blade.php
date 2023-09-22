92042bf273837ea42505f073bb5286a073faa9e9
Jagad R R <43224845+zenthicmc@users.noreply.github.com>
2023-09-22 22:10:37 +0700

    Update overview.blade.php

--- resources/views/dashboard/overview.blade.php
+++ resources/views/dashboard/overview.blade.php
@@ -43,7 +43,7 @@
 							</div>
 							<div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
 							<h6 class="text-muted font-semibold">
-								Pendapatan
+								Total Pendapatan
 							</h6>
 							<h6 class="font-extrabold mb-0">@currency($total_income)</h6>
 							</div>
@@ -61,7 +61,7 @@
 									</div>
 								</div>
 								<div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-									<h6 class="text-muted font-semibold">Transaksi</h6>
+									<h6 class="text-muted font-semibold">Total Transaksi</h6>
 									<h6 class="font-extrabold mb-0">{{ count($transactions) }}</h6>
 								</div>
 							</div>
@@ -78,7 +78,7 @@
 									</div>
 								</div>
 								<div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-									<h6 class="text-muted font-semibold">Produk</h6>
+									<h6 class="text-muted font-semibold">Total Produk</h6>
 									<h6 class="font-extrabold mb-0">{{ count($products) }}</h6>
 								</div>
 							</div>
