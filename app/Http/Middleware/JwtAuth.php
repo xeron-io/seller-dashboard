4faaa1f4a54d5d08f0223496a45e8884ee88cb14
Jagad R R <43224845+zenthicmc@users.noreply.github.com>
2023-09-23 08:54:34 +0700

    Update JwtAuth.php

--- app/Http/Middleware/JwtAuth.php
+++ app/Http/Middleware/JwtAuth.php
@@ -6,6 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 use App\Http\Controllers\AuthController;
+use App\Models\Sellers;
 
 class JwtAuth
 {
@@ -14,6 +15,12 @@ class JwtAuth
       if(!$request->session()->has('token')) return redirect()->route('login');
       if(is_null(AuthController::getJWT())) return redirect()->route('login');
       if(!AuthController::getJWT()) return redirect()->route('login');
+
+      $seller = Sellers::where('id', AuthController::getJWT()->sub)->first();
+      if($seller->status == 'suspended') {
+         Session::flush();
+         return redirect()->route('login')->withErrors(['email' => 'Akun anda telah di suspend.']);
+      }
       return $next($request);
    }  
 }
