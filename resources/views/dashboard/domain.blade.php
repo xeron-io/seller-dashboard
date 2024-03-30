1545cbc90de7cd5ceb97b3fc785191d1ae3a2c92
Jagad R R <43224845+zenthicmc@users.noreply.github.com>
2024-03-30 20:37:56 +0700

    Update domain.blade.php

--- resources/views/dashboard/domain.blade.php
+++ resources/views/dashboard/domain.blade.php
@@ -73,7 +73,7 @@
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h4 class="modal-title" id="myModalLabel33">
+          <h4 class="modal-title text-white" id="myModalLabel33">
             Tambah Custom Domain
           </h4>
           <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
